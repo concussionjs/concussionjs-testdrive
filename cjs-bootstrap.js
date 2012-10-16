f038a8b1422d4e8c7af00b869d1bff64eb413427
frenb <ben@concussionjs.com>
2012-10-16 09:15:19 -0400

    modified comments

--- cjs-bootstrap.js
+++ cjs-bootstrap.js
@@ -1,6 +1,5 @@
 /*
-	Library to leverage the power of the concussion platform from anywhere
-	
+	Library to leverage the power of the concussion platform from anywhere	
 */
 
 //$.getScript("http://testdrive.concussionjs.com/assets/js/libs/jquery-1.7.1.min.js",function(){
